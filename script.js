5d6caf6766a24219b73bdce221d3557b1ce5e4eb
dprebyl <drakeprebyl@gmail.com>
2020-03-24 15:06:05 -0500

    Implement easy AI

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Rows are counted from the bottom up (bottom row = 1)
-const ROWS = 6;
+const ROWS = 6; // Rows are counted from the bottom up (bottom row = 1)
 const COLS = 7;
+const WIN_SCORE = 4; // How many in a row to win
+
+// Minimum time in ms
+const AI_THINK_TIME = 600;
 
 // Creates dropdowns in the option box
 const SETTINGS = [
@@ -13,12 +16,9 @@ const SETTINGS = [
 		"id": "opponent",
 		"name": "Opponent",
 		"changeCallback": setOpponent,
-		"options": ["Human", "Easy AI", "Hard AI"]
+		"options": ["Human", "Easy AI", /*"Hard AI"*/]
 	}
 ];
-
-// Players are 0 and 1 (false and true)
-let curPlayer = false;
 
 const GAME_STATES = {
 	"NOT_STARTED": 0,
@@ -26,6 +26,12 @@ const GAME_STATES = {
 	"WON": 2
 }
 let gameState = GAME_STATES.NOT_STARTED;
+
+// Players are 0 and 1 (false and true), 0 is always human and goes first
+let curPlayer = false;
+
+// Set to a function when playing against AI
+let aiMoveFunc = null;
 
 window.addEventListener("DOMContentLoaded", () => {
 	setPlayer(false);
@@ -46,10 +52,11 @@ window.addEventListener("DOMContentLoaded", () => {
 		board.appendChild(tr);
 	}
 
-	var settingsDiv = document.getElementById("settings");
+	let settingsDiv = document.getElementById("settings");
 	for (let setting of SETTINGS) {
 		settingsDiv.appendChild(document.createTextNode(setting.name + ": "));
 		let dropdown = document.createElement("select");
+		dropdown.id = setting.id;
 		dropdown.addEventListener("change", () => {
 			localStorage.setItem(setting.id, dropdown.value);
 			setting.changeCallback(dropdown.value)
@@ -77,44 +84,111 @@ function setTheme(theme) {
 function setOpponent(opponent) {
 	if (gameState === GAME_STATES.IN_PROGRESS) 
 		return msg("New opponent next game");
+	
+	// Function name in the format of opponent + "Move", ex: "easy_aiMove"
+	aiMoveFunc = (opponent == "human" ? null : window[opponent + "Move"]);
 }
 
 function clickCol(col) {
+	// Disable clicking while AI thinking
+	if (aiMoveFunc === null || !curPlayer || gameState === GAME_STATES.WON) placeMove(col);
+}
+
+function placeMove(col) {
 	if (gameState == GAME_STATES.WON) return resetGame();
-	// Find the lowest empty space in the column clicked
-	for (let row = 1; row <= ROWS; row++) {
-		if (getChip(row, col) === "blank") {
-			setChip(row, col, curPlayerClass());
-			if (checkWin(row, col)) {
-				msg("player wins!");
-				document.body.classList.add("win");
-				gameState = GAME_STATES.WON;
-			}
-			else {
-				msg("player's turn");
-				setPlayer(!curPlayer);
-				gameState = GAME_STATES.IN_PROGRESS;
-			}
-			return;
-		}
+	let row = lowestEmptyRow(col);
+	if (row === -1) return msg("Invalid - column full");
+	
+	setChip(row, col, curPlayerClass());
+	if (checkWin(row, col)) {
+		msg("player wins!");
+		document.body.classList.add("win");
+		gameState = GAME_STATES.WON;
 	}
-	msg("Invalid - column full");
+	else if (aiMoveFunc === null || curPlayer) { // Switch to other player
+		msg("player's turn");
+		setPlayer(!curPlayer);
+		gameState = GAME_STATES.IN_PROGRESS;
+	}
+	else { // Start AI's move
+		msg("AI thinking...");
+		setPlayer(!curPlayer);
+		setTimeout(aiMoveFunc, AI_THINK_TIME);
+	}
+}
+
+/*
+	The easy AI's strategy is as follows:
+	- If there is somewhere it can go that directly prevents the opponent from getting 4 in a row next turn it will go in the first one it finds.
+	- If not, it will go in a space that creates the longest row of its own pieces.
+	- If multiple places yield the same longest row it picks a random one.
+	- It will not pick a place that allows the opponent to make 4 in a row next turn, unless doing so lets it win.
+	- To beat the AI, create two ways for you to get 4 in a row in the next turn.
+*/
+function easy_aiMove() {
+	let colScores = [];
+	for (let col = 1; col <= COLS; col++) {
+		let row = lowestEmptyRow(col);
+		if (row === -1) { // Skip full columns
+			colScores[col] = -1;
+			continue; 
+		}
+		
+		// Check if the human would win by placing their piece here next turn - block them if so
+		if (canPlayer0Win(row, col)) return placeMove(col);
+		
+		// Test how good placing in this column would be
+		setChip(row, col, curPlayerClass());
+		colScores[col] = score(row, col);
+		// Check if the AI placing here would allow the human to win next turn by placing on top of it
+		if (row < ROWS && canPlayer0Win(row+1, col) && colScores[col] < WIN_SCORE) {
+			colScores[col] = 0; // Only go here if it's the only valid move
+		}
+		setChip(row, col, "blank");
+	}
+	
+	// Click in a random column with the best score
+	let bestScore = Math.max(...colScores.slice(1));
+	let possibleMoves = [];
+	for (let col = 1; col <= COLS; col++)
+		if (colScores[col] === bestScore) possibleMoves.push(col);
+	placeMove(possibleMoves[Math.floor(Math.random()*possibleMoves.length)]);
+	
+	// See if the human player can win in this space
+	function canPlayer0Win(row, col) {
+		curPlayer = !curPlayer;
+		setChip(row, col, curPlayerClass());
+		let wouldWin = checkWin(row, col);
+		curPlayer = !curPlayer;
+		setChip(row, col, "blank");
+		return wouldWin;
+	}
+}
+
+function hard_aiMove() {
+	msg("NYI");
 }
 
 function checkWin(row, col) {
-	return checkDir(0, 1) || checkDir(1, 0) || checkDir(1, 1) || checkDir(1, -1);
+	return score(row, col) >= WIN_SCORE;
+}
+
+// Returns the largest number of pieces in a row the current player has relative to this space (up to 2*WIN_SCORE-1)
+function score(row, col) {
+	return Math.max(checkDir(0, 1), checkDir(1, 0), checkDir(1, 1), checkDir(1, -1));
 
 	function checkDir(incRow, incCol) {
-		var count = 0;
-		for (var i = -3; i <= 3; i++) {
+		let maxCount = 0;
+		let count = 0;
+		for (let i = 1-WIN_SCORE; i <= WIN_SCORE-1; i++) {
 			if (getChip(row+incRow*i, col+incCol*i) === curPlayerClass()) {
 				count++;
-				if (count >= 4) return true;
+				if (count >= maxCount) maxCount = count;
 			} else {
 				count = 0;
 			}
 		}
-		return false;
+		return maxCount;
 	}
 }
 
@@ -123,8 +197,15 @@ function resetGame() {
 	let chips = document.querySelectorAll("#board td");
 	for (let chip of chips) chip.className = "blank";
 	gameState = GAME_STATES.NOT_STARTED;
-	curPlayer = false;
+	setPlayer(false);
+	setOpponent(document.getElementById("opponent").value); // In case opponent was changed mid-game
 	msg("goes first");
+}
+
+function lowestEmptyRow(col) {
+	for (let row = 1; row <= ROWS; row++)
+		if (getChip(row, col) === "blank") return row;
+	return -1; // Column full
 }
 
 function getChip(row, col) {
