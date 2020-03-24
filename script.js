7b432b5389bf181a087f6c00b14ae0beca88af31
dprebyl <drakeprebyl@gmail.com>
2020-03-24 13:25:29 -0500

    Improve settings

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 const ROWS = 6;
 const COLS = 7;
 
-// First theme is default
-const THEMES = ["KU", "Traditional"];
+// Creates dropdowns in the option box
+const SETTINGS = [
+	{
+		"id": "theme",
+		"name": "Theme",
+		"changeCallback": setTheme,
+		"options": ["KU", "Traditional"]
+	}, {
+		"id": "opponent",
+		"name": "Opponent",
+		"changeCallback": setOpponent,
+		"options": ["Human", "Easy AI", "Hard AI"]
+	}
+];
 
 // Players are 0 and 1 (false and true)
 let curPlayer = false;
+
+const GAME_STATES = {
+	"NOT_STARTED": 0,
+	"IN_PROGRESS": 1,
+	"WON": 2
+}
+let gameState = GAME_STATES.NOT_STARTED;
 
 window.addEventListener("DOMContentLoaded", () => {
 	setPlayer(false);
@@ -26,38 +45,54 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 		board.appendChild(tr);
 	}
-	
-	// Initialize theme selector
-	let themeSelector = document.getElementById("theme-selector");
-	themeSelector.addEventListener("change", () => setTheme(themeSelector.value));
-	for (let theme of THEMES) {
-		let option = document.createElement("option");
-		option.value = theme.toLowerCase();
-		option.innerText = theme;
-		if (localStorage.getItem("theme") === theme.toLowerCase()) {
-			option.selected = "selected";
-			setTheme(theme.toLowerCase());
+
+	var settingsDiv = document.getElementById("settings");
+	for (let setting of SETTINGS) {
+		settingsDiv.appendChild(document.createTextNode(setting.name + ": "));
+		let dropdown = document.createElement("select");
+		dropdown.addEventListener("change", () => {
+			localStorage.setItem(setting.id, dropdown.value);
+			setting.changeCallback(dropdown.value)
+		});
+		for (let optionName of setting.options) {
+			let option = document.createElement("option");
+			// Replace all non-alphanumeric characters with _
+			option.value = optionName.toLowerCase().replace(/[^a-z0-9]/g,'_');
+			option.innerText = optionName;
+			if (localStorage.getItem(setting.id) === option.value) {
+				option.selected = "selected";
+				setting.changeCallback(option.value);
+			}
+			dropdown.appendChild(option);
 		}
-		themeSelector.appendChild(option);
+		settingsDiv.appendChild(dropdown);
+		settingsDiv.appendChild(document.createElement("br"));
 	}
 });
 
 function setTheme(theme) {
 	document.getElementById("theme").href = "theme-" + theme + "/style.css";
-	localStorage.setItem("theme", theme);
+}
+
+function setOpponent(opponent) {
+	if (gameState === GAME_STATES.IN_PROGRESS) 
+		return msg("New opponent next game");
 }
 
 function clickCol(col) {
+	if (gameState == GAME_STATES.WON) return resetGame();
 	// Find the lowest empty space in the column clicked
 	for (let row = 1; row <= ROWS; row++) {
 		if (getChip(row, col) === "blank") {
 			setChip(row, col, curPlayerClass());
 			if (checkWin(row, col)) {
 				msg("player wins!");
+				gameState = GAME_STATES.WON;
 			}
 			else {
 				msg("player's turn");
 				setPlayer(!curPlayer);
+				gameState = GAME_STATES.IN_PROGRESS;
 			}
 			return;
 		}
@@ -80,6 +115,14 @@ function checkWin(row, col) {
 		}
 		return false;
 	}
+}
+
+function resetGame() {
+	let chips = document.querySelectorAll("#board td");
+	for (let chip of chips) chip.className = "blank";
+	gameState = GAME_STATES.NOT_STARTED;
+	curPlayer = false;
+	msg("goes first");
 }
 
 function getChip(row, col) {
