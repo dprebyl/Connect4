311e0f18ccb58ad95ef0289d722a7a5aa57632e0
dprebyl <drakeprebyl@gmail.com>
2020-04-20 10:53:35 -0500

    Add minimal version

---
+++ minimal/README.md
@@ -0,0 +1,9 @@
+# https://dprebyl.github.io/Connect4/
+
+This is my final JavaScript project for the EECS 368 (Programming Language Paradigms) class at KU.
+
+Features:
+- Play against another person or an AI
+- Detects wins, full columns, and full boards
+- Selectable themes complete with win animations
+- Hotkeys (1-7) for quickly placing pieces
---
+++ minimal/script.js
@@ -0,0 +1,170 @@
+const ROWS = 6; // Rows are counted from the bottom up (bottom row = 1)
+const COLS = 7;
+const WIN_SCORE = 4; // How many in a row to win
+
+const GAME_STATES = {
+	"NOT_STARTED": 0,
+	"IN_PROGRESS": 1,
+	"WON": 2
+}
+let gameState = GAME_STATES.NOT_STARTED;
+
+// Players are false and true, false is always human and goes first
+let curPlayer = false;
+
+// 2D array of the pieces on the board
+let chips = [];
+
+// Chip constructor
+function Chip(elementIn) {
+	let element = elementIn;
+	let type = "blank";
+	this.getType = function() { 
+		return type;
+	}
+	// Valid types: "blank", false (player0), or true (player1)
+	this.setType = function(newType) {
+		type = newType;
+		element.className = typeof type === "boolean" ? "player" + +type : type;
+	}
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	setPlayer(false);
+	
+	// Initialize board and chips array
+	let board = document.getElementById("board");
+	for (let row = ROWS; row >= 1; row--) {
+		let tr = document.createElement("tr");
+		chips[row-1] = [];
+		for (let col = 1; col <= COLS; col++) {
+			let td = document.createElement("td");
+			td.className = "blank";
+			let chip = document.createElement("button");
+			chip.className = "chip";
+			chip.addEventListener("click", () => { clickCol(col); });
+			td.appendChild(chip);
+			tr.appendChild(td);
+			chips[row-1][col-1] = new Chip(td);
+		}
+		board.appendChild(tr);
+	}
+
+	// Initialize settings
+	let settingsDiv = document.getElementById("settings");
+	for (let setting of SETTINGS) {
+		settingsDiv.appendChild(document.createTextNode(setting.name + ": "));
+		let dropdown = document.createElement("select");
+		dropdown.id = setting.id;
+		dropdown.addEventListener("change", () => {
+			localStorage.setItem(setting.id, dropdown.value);
+			setting.changeCallback(dropdown.value)
+		});
+		for (let optionId in setting.options) {
+			let option = document.createElement("option");
+			option.value = optionId;
+			option.innerText = setting.options[optionId];
+			if (localStorage.getItem(setting.id) === option.value) {
+				option.selected = "selected";
+				setting.changeCallback(option.value);
+			}
+			dropdown.appendChild(option);
+		}
+		settingsDiv.appendChild(dropdown);
+		settingsDiv.appendChild(document.createElement("br"));
+	}
+});
+
+function clickCol(col) {
+	if (gameState == GAME_STATES.WON) return resetGame();
+	let row = lowestEmptyRow(col);
+	if (row === -1) return msg("Invalid - column full");
+	
+	setChip(row, col, curPlayer);
+	if (checkWin(row, col)) {
+		msg("player wins!");
+		document.body.classList.add("win");
+		gameState = GAME_STATES.WON;
+	}
+	else {
+		// Check if the board is full
+		for (let col = 1; col <= COLS; col++) {
+			if (lowestEmptyRow(col) !== -1) break;
+			else if (col == COLS) {
+				msg("Full board - tie game!");
+				gameState = GAME_STATES.WON;
+				return;
+			}
+		}
+		
+		// Switch to other player's turn
+		if (aiMoveFunc === null || curPlayer) { 
+			msg("player's turn");
+			setPlayer(!curPlayer);
+			gameState = GAME_STATES.IN_PROGRESS;
+		}
+		else { // Start AI's move
+			msg("AI thinking...");
+			setPlayer(!curPlayer);
+			setTimeout(aiMoveFunc, AI_THINK_TIME);
+		}
+	}
+}
+
+function checkWin(row, col) {
+	return score(row, col) >= WIN_SCORE;
+}
+
+// Returns the largest number of pieces in a row the current player has relative to this space 
+// Not guarenteed to be the max possible when return value >= WIN_SCORE
+function score(row, col) {
+	return Math.max(checkDir(0, 1), checkDir(1, 0), checkDir(1, 1), checkDir(1, -1));
+
+	function checkDir(incRow, incCol) {
+		let maxCount = 0;
+		let count = 0;
+		for (let i = 1-WIN_SCORE; i <= WIN_SCORE-1; i++) {
+			if (getChip(row+incRow*i, col+incCol*i) === curPlayer) {
+				count++;
+				if (count >= maxCount) maxCount = count;
+			} else {
+				count = 0;
+			}
+		}
+		return maxCount;
+	}
+}
+
+function resetGame() {
+	document.body.classList.remove("win");
+	for (let chipRow of chips) for (let chip of chipRow) chip.setType("blank");
+	gameState = GAME_STATES.NOT_STARTED;
+	setPlayer(false);
+	setOpponent(document.getElementById("opponent").value); // In case opponent was changed mid-game
+	msg("goes first");
+}
+
+function lowestEmptyRow(col) {
+	for (let row = 1; row <= ROWS; row++)
+		if (getChip(row, col) === "blank") return row;
+	return -1; // Column full
+}
+
+function getChip(row, col) {
+	if (row < 1 || col < 1 || row > ROWS || col > COLS) return undefined;
+	return chips[row-1][col-1].getType();
+}
+
+function setChip(row, col, type) {
+	if (row < 1 || col < 1 || row > ROWS || col > COLS) return;
+	chips[row-1][col-1].setType(type);
+}
+
+function setPlayer(newPlayer) {
+	curPlayer = newPlayer;
+	document.getElementById("current-player").className = "player" + +curPlayer;
+}
+
+function msg(m) {
+	document.getElementById("msg").innerText = m;
+}
